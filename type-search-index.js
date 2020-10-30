b182d1ab9ed8192b53bf9daf68efd1fd6b0a86f6
TheQuetz <68091477+TheQuetz@users.noreply.github.com>
2020-10-29 21:31:50 -0600

    Add files via upload

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"proyecto.ordenamiento.externo","l":"Menu"},{"p":"proyecto.ordenamiento.externo","l":"ProyectoOrdenamientoExterno"}];updateSearchResults();
